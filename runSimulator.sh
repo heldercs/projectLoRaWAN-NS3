fbd63c2c37811477f8a85366682391580bc83a68
heldercs <helderhdw@gmail.com>
2018-10-05 14:15:27 -0300

    commit run script and git iginore

--- runSimulator.sh
+++ runSimulator.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $# -ne 6 ]
+if [ $# -ne 7 ]
 then
 echo "parameters missing"
 exit 1
@@ -11,7 +11,8 @@ rad=$2
 gwRad=$3
 simTime=$4
 interval=$5
-pEDs=$6
+trial=$6
+pEDs=$7
 
 
 echo "##### Simulation Start #####"
@@ -21,43 +22,41 @@ if [ ! -d TestResult/ ]
 	mkdir TestResult/
 fi
 
-trial=0;
-#for trial in 1 2 3
-#do
-	if [ ! -d TestResult/test$trial/ ]
+if [ ! -d TestResult/test$trial/ ]
+	then
+	mkdir TestResult/test$trial/
+fi
+
+if [ ! -d TestResult/test$trial/traffic-$interval/ ]
+	then
+	mkdir TestResult/test$trial/traffic-$interval/
+fi
+
+touch ./TestResult/test$trial/traffic-$interval/mac-STAs-GW-$gwRing.txt
+file1="./TestResult/test$trial/traffic-$interval/mac-STAs-GW-$gwRing.txt"
+
+touch ./TestResult/test$trial/traffic-$interval/result-STAs.dat
+file2="./TestResult/test$trial/traffic-$interval/result-STAs.dat"
+echo "#numSta    Throughput(Kbps)    ProbSucc(%)    ProbLoss(%)" > ./TestResult/test$trial/traffic-$interval/result-STAs.dat 
+
+for numSta in 100 200 300 400 500 600 700 800 900 1000 1100 1200 1300 1400 1500 1600 1700 1800 1900 2000
+do
+		echo "trial:$trial-numSTA:$numSta"
+
+		if [ ! -d TestResult/test$trial/traffic-$interval/pcap-sta-$numSta/ ]
 		then
-		mkdir TestResult/test$trial/
-	fi
+			mkdir TestResult/test$trial/traffic-$interval/pcap-sta-$numSta/
+		fi
 
-	if [ ! -d TestResult/test$trial/traffic-$interval/ ]
-		then
-		mkdir TestResult/test$trial/traffic-$interval/
-	fi
+		touch TestResult/test$trial/time-record$numSta.txt
 
-	touch ./TestResult/test$trial/traffic-$interval/mac-STAs-GW-$gwRing.txt
-	file1="./TestResult/test$trial/traffic-$interval/mac-STAs-GW-$gwRing.txt"
+		echo "Time: $(date) $interval $numSta" >> TestResult/test$trial/time-record$numSta.txt
 
-	touch ./TestResult/test$trial/traffic-$interval/result-STAs.dat
-	file2="./TestResult/test$trial/traffic-$interval/result-STAs.dat"
-	echo "#numSta    Throughput(Kbps)    ProbSucc(%)    ProbLoss(%)" > ./TestResult/test$trial/traffic-$interval/result-STAs.dat 
+  	./waf --run "lorawan-network-sim --nDevices=$numSta --gatewayRings=$gwRing --radius=$rad -gatewayRadius=$gwRad --simulationTime=$simTime --appPeriod=$interval --printEDs=$pEDs --file1=$file1 --file2=$file2 --trial=$trial"  > ./TestResult/test$trial/traffic-$interval/pcap-sta-$numSta/record-$numSta.txt 2>&1
 
-	for numSta in 100 200 300 400 500 600 700 800 900 1000 1100 1200 1300 1400 1500 1600 1700 1800 1900 2000
-	do
-			echo "trial:$trial-numSTA:$numSta"
-
-			if [ ! -d TestResult/test$trial/traffic-$interval/pcap-sta-$numSta/ ]
-			then
-				mkdir TestResult/test$trial/traffic-$interval/pcap-sta-$numSta/
-			fi
-
-			touch TestResult/test$trial/time-record$numSta.txt
-
-			echo "Time: $(date) $interval $numSta" >> TestResult/test$trial/time-record$numSta.txt
-
-	  	./waf --run "lorawan-network-sim --nDevices=$numSta --gatewayRings=$gwRing --radius=$rad -gatewayRadius=$gwRad --simulationTime=$simTime --appPeriod=$interval --printEDs=$pEDs --file1=$file1 --file2=$file2 --trial=$trial"  > ./TestResult/test$trial/traffic-$interval/pcap-sta-$numSta/record-$numSta.txt 2>&1
-
-	done
-#done
+done
 echo "##### Simulation finish #####"
+echo "seinding email..."
+echo simulator finish | mail -s Simulator helderhdw@gmail.com
 
 
